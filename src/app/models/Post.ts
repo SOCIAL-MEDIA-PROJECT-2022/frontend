4b0144ebadf98e48b0e4281e60824730d385c386
PatrickShawnNelson <100812711+PatrickShawnNelson@users.noreply.github.com>
2022-09-13 15:46:30 -0400

    added likes into the post model

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -5,13 +5,15 @@ export default class Post {
     text: string
     imageUrl: string
     author: User
+    likes : number
     comments: Post[]
 
-    constructor (id: number, text: string, imageUrl: string, author: User, comments: Post[]) {
+    constructor (id: number, text: string, imageUrl: string, author: User, likes: number, comments: Post[]) {
         this.id = id
         this.text = text
         this.imageUrl = imageUrl
         this.author = author
+        this.likes = likes
         this.comments = comments
     }
 }
