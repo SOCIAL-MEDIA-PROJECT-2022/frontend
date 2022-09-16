e2aed1a684aec547eb26478983121c135d889206
PatrickShawnNelson <100812711+PatrickShawnNelson@users.noreply.github.com>
2022-09-16 11:01:52 -0400

    front end works with backend

--- src/app/components/likes/likes.component.ts
+++ src/app/components/likes/likes.component.ts
@@ -23,15 +23,16 @@ export class LikesComponent implements OnInit {
   likePost = () => {
     //fetch post id and user email
     this.likes = {
-      id: 10000,
+      id: this.postID,
       //email: this.authService.currentUser.email
-      id2: 1
+      id2: this.authService.currentUser.id
     }
-    console.log(this.postID)
+   /* console.log(this.postID)
     console.log("This was our likes object")
-
+    console.log("id: "  + this.likes.id)
+    console.log("id2: "  + this.likes.id2)*/
     //sending a post ID with the name of the user that liked it
-    this.likeService.updateLikes(this.likes).subscribe()
+    this.likeService.updateLikes(this.likes.id, this.likes.id2).subscribe()
     
     
     
--- src/app/services/likes.service.ts
+++ src/app/services/likes.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
+import { PostComponent } from '../components/post/post.component';
 import Likes from '../models/Likes';
 import Post from '../models/Post';
 
@@ -14,7 +15,8 @@ export class LikesService {
 
   constructor(private http: HttpClient) { }
 
-  updateLikes(likes : Likes): Observable<Likes> {
-    return this.http.patch<Likes>(`${this.postUrl}`, likes, {headers: environment.headers, withCredentials: environment.withCredentials})
+  updateLikes(id: number, id2: number): Observable<Post> {
+    const likes = {postId:id, userId:id2};
+    return this.http.post<Post>(`${this.postUrl}/like`, likes, {headers: environment.headers, withCredentials: environment.withCredentials})
   }
 }
