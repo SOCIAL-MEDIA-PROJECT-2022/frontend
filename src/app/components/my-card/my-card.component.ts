4b031ffa02e4293d7a6285ab6d42d347f7a24278
JD Megargel <109110533+MegaJam750@users.noreply.github.com>
2022-09-18 17:15:33 -0700

    adding 10 profile JPG pics to match 10 users in system

--- src/app/components/my-card/my-card.component.ts
+++ src/app/components/my-card/my-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Profile from 'src/app/models/Profile';
+import User from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-my-card',
@@ -7,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MyCardComponent implements OnInit {
+  user: User = {} as User;
+  profile: Profile = {} as Profile;
 
-
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private profileService: ProfileService
+  ) { }
 
   ngOnInit(): void {
+    this.user = this.authService.currentUser;
   }
 }
