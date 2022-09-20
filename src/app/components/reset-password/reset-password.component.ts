6b773a9e5ed6ac78e4894cf73700c2151cfcab9c
TazrianH <tazrianh98@gmail.com>
2022-09-20 17:45:51 -0400

    frontend for resetting password

---
+++ src/app/components/reset-password/reset-password.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import User from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css'],
+})
+export class ResetPasswordComponent implements OnInit {
+  user: User = {} as User;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  resetForm = new FormGroup({
+    npassword: new FormControl(''),
+    cpassword: new FormControl(''),
+  });
+  error_message: string;
+
+  ngOnInit(): void {
+    this.user = this.authService.currentUser;
+  }
+  onSubmit(e: any): void {
+    e.preventDefault();
+    if (this.resetForm.value.npassword === this.resetForm.value.cpassword) {
+      console.log(this.user.email);
+      this.authService.resetpassword(
+        this.user.email,
+        this.resetForm.value.npassword || ''
+      );
+    } else {
+      this.resetForm.setErrors({ error: true });
+      this.error_message = "They don't match";
+    }
+  }
+}
