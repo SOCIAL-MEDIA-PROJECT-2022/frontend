3a4adab9b1476ae0e8528aa1d6e71204b9b9708c
jaylenbyrd <jaybyrd1194@live.com>
2022-09-22 17:55:06 -0400

    added aboutMe and proPict to user model. Now user is able to edit about me instead of email

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -3,11 +3,16 @@ export default class User {
     email: string
     firstName: string
     lastName: string
+    proPict: string
+    aboutMe: string
 
-    constructor (id: number, email: string, firstName: string, lastName: string) {
+
+    constructor (id: number, email: string, firstName: string, lastName: string, proPict: string, aboutMe: string) {
         this.id = id
         this.email = email
         this.firstName = firstName
         this.lastName = lastName
+        this.proPict = proPict
+        this.aboutMe = aboutMe
     }
 }
